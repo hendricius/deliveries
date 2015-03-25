3888825a4567ecb18f2431cf26907f9b0348472e
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2015-03-25 22:55:53 +0100

    temp commit

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,12 @@
 class Driver < ActiveRecord::Base
   belongs_to :company
+
+  def coordinates
+    # FIXME implement
+  end
+
+  def distance_to_order?
+    # FIXME real implementation
+    Geocoder::Calculations.distance_between([47.858205,2.294359], [40.748433,-73.985655])
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ActiveRecord::Base
   geocoded_by :from_address, latitude: :from_latitude, longitude: :from_longitude
   geocoded_by :to_address, latitude: :to_latitude, longitude: :to_longitude
   after_validation :geocode
+
+  def coordinates
+    # FIXME implement
+  end
 end
